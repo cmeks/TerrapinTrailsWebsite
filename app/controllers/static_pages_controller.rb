c12c9298454a65556f664e624f06f234d89880e9
cmeks <cmekss@gmail.com>
2015-02-09 23:39:08 -0500

    added small changes to the legal element and front page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,13 @@ class StaticPagesController < ApplicationController
 	before_action :correct_user, only: [:change_role]
 
 	def home
+		@trips = Trip.all
+		@current = Array.new
+		@trips.each do |trip|
+			if trip.start_date > Time.now && trip.end_date > Time.now
+				@current.push(trip)
+			end
+		end
 		render :layout => 'home'
 	end
 
