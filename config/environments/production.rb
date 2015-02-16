0937a124e872eb5e4ebda5efdefebcdfcb5a11ca
cmeks <cmekss@gmail.com>
2015-02-16 17:18:58 -0500

    fixed an env bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,8 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'terrapintraildemo.herokuapp.com'
+  host = 'terrapintrailclub.org'
+  #host = 'terrapintraildemo.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
