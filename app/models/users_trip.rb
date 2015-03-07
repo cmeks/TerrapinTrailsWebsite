9fc3a199df5a68c904390dc0363abcbf5275aad4
cmeks <cmekss@gmail.com>
2015-03-07 14:18:07 -0500

    order user_trips by time they were created at

--- app/models/users_trip.rb
+++ app/models/users_trip.rb
@@ -3,6 +3,9 @@ class UsersTrip < ActiveRecord::Base
   	belongs_to :user
   	belongs_to :trip
   	has_many :users_cars, dependent: :destroy
+    
+    #make sure user_trips are ordered by the time they were created at
+    default_scope -> { order(created_at: :asc) }
 
   	#validations
   	validates :user_id, presence: true
